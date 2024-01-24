80911aa10a1c701fd651d1e5fab8c5d2b55259c5
Winfred <winfrednginakilonzo@gmail.com>
2024-01-25 00:35:53 +0300

    Move reviewed math game codebase

---
+++ MathGame.WinnieNgina/MathGame.WinnieNgina/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MathGame.WinnieNgina.Models;
+internal class Game
+{
+    public int Score { get; set; }
+    public DateTime Date { get; set; }
+    public GameType Type { get; set; }
+    public DifficultLevel Level { get; set; }
+
+}
+internal enum GameType
+{
+    Addition,
+    Subtraction,
+    Multiplication,
+    Division
+}
+internal enum DifficultLevel
+{
+    Easy,
+    Medium,
+    Difficult
+}
+
---
+++ MathGame.WinnieNgina/MathGame.WinnieNgina/Program.cs
@@ -0,0 +1,6 @@
+﻿using MathGame.WinnieNgina;
+var menu = new Menu();
+var date = DateTime.UtcNow;
+string name = Helpers.GetName();
+menu.ShowMenu(name, date);
+
